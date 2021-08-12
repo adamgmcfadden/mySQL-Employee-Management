f34f558438cfeff258fb525b7dc53b4025ab9eb8
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-08-12 13:26:14 -0400

    add employee function complete

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const manageEmployees = () => {
       } else if (interface === "View All Employees By Manager") {
         return viewAllByManager();
       } else if (interface === "Add Employee") {
-        return "Add Employee";
+        return addNewEmployee();
       } else if (interface === "Remove Employee") {
         return "Remove Employee";
       } else if (interface === "Update Employee Role") {
@@ -86,53 +86,67 @@ const viewAllEmployees = () => {
 
 // view employees by Department
 const viewAllByDepartment = () => {
+  //mysql query : Select All from department table
   db.query(`SELECT * FROM department`, (err, res) => {
     if (err) {
       console.log(err);
     }
+    //create new array with key and value for each department
     const chooseDept = res.map(({ id, dept_name }) => ({
       name: dept_name,
       value: id,
     }));
 
+    //prompt user with a list of departments to choose from
     inquirer
       .prompt({
         type: "list",
         name: "department",
         message: "Which department would you like to see?",
+        //call chooseDept array for choices
         choices: chooseDept,
       })
       .then((choice) => {
+        //deconstruct "name:" and save as choice parameter
         const { department } = choice;
+        //save SQL query as sql
         const sql = `SELECT CONCAT(first_name," ",last_name) AS Name
                   FROM employee
                     LEFT JOIN role ON role.id = employee.role_id
                     LEFT JOIN department ON department.id = role.department_id
                     WHERE department_id=?`;
 
+        //mysql query
         db.query(sql, department, (err, res) => {
           if (err) {
             console.log(err);
           }
+          //construct table in node.js from query
           console.table(res);
+          //call initializing function to return to inquirer prompt
           manageEmployees();
         });
       });
   });
 };
 
+//view employees by their manager
 const viewAllByManager = () => {
-  const sql = `SELECT * FROM employee WHERE manager_id = 36`;
+  //save sql query - SELECT all from employee where manager ID = 36 OR NULL (see db/seeds - will make sense)
+  const sql = `SELECT * FROM employee WHERE manager_id = 36 OR manager_id is NULL`;
 
+  //database query
   db.query(sql, (err, res) => {
     if (err) {
       console.log(err);
     }
+    //create new array with key and value for each manager
     const chooseManager = res.map(({ id, first_name, last_name }) => ({
       name: first_name + " " + last_name,
       value: id,
     }));
 
+    //prompt user with list of managers to choose from
     inquirer
       .prompt({
         type: "list",
@@ -141,15 +155,151 @@ const viewAllByManager = () => {
         choices: chooseManager,
       })
       .then((choice) => {
+        //deconstruct "name:" and save as choice parameter
         const { managerList } = choice;
+        //save sql query to be used : concat first and last name of employee where manager id=?
         const sql = `SELECT CONCAT(first_name," ",last_name) AS Name FROM employee WHERE manager_id=?`;
         db.query(sql, managerList, (err, res) => {
           if (err) {
             console.log(err);
           }
+          //construct table in node.js from query
           console.table(res);
+          //call initializing function to return to inquirer prompt
           manageEmployees();
         });
       });
   });
+};
+
+//function to add new employee
+const addNewEmployee = () => {
+  //prompt user with question to generate new employee in database
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "Please enter the new employee's first name.",
+        //validate - a value must be entered
+        validate: (name) => {
+          if (name) {
+            return true;
+          } else {
+            console.log("The employee's first name must be entered!");
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "Please enter the new employee's last name.",
+        //validate - a value must be entered
+        validate: (name) => {
+          if (name) {
+            return true;
+          } else {
+            console.log("The employee's last name must be entered!");
+          }
+        },
+      },
+    ])
+    .then((names) => {
+      //deconstruct "name:" and save as choice parameter
+      const newEmplArray = [names.first_name, names.last_name];
+      //query to select role id and title from role table
+      const sql = `SELECT role.id, role.title FROM role`;
+      //databse query
+      db.query(sql, (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+        //create new array with key and value for role to choose from
+        const chooseRole = res.map(({ id, title }) => ({
+          name: title,
+          value: id,
+        }));
+
+        //prompt user with list of roles available in database
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "role",
+              message: "Select a role ",
+              //choices : chooseRole array
+              choices: chooseRole,
+            },
+          ])
+
+          .then((choice) => {
+            //save choice.role as empRole
+            const role = choice.role;
+            //push role to employee array
+            newEmplArray.push(role);
+            //save sql query - choose list of managers
+            const sql = `SELECT * FROM employee WHERE manager_id = 36 OR manager_id is NULL`;
+            //sql query
+            db.query(sql, (err, res) => {
+              if (err) {
+                console.log(err);
+              }
+              //create new array with key and value for role to choose from
+              const chooseManager = res.map(
+                ({ id, first_name, last_name }) => ({
+                  name: first_name + " " + last_name,
+                  value: id,
+                })
+              );
+
+              //prompt user with a list of managers to choose from if new Employee has a manager
+
+              inquirer
+                .prompt([
+                  {
+                    //does new employee have a manager?
+                    type: "confirm",
+                    name: "addManager",
+                    message: "Does the new Employee have a manager?",
+                    default: false,
+                  },
+                  {
+                    type: "list",
+                    name: "managers",
+                    message: "Please select from this list of managers",
+                    choices: chooseManager,
+                    //only prompted when 1st prompt is true
+                    when: function (managers) {
+                      return managers.addManager === true;
+                    },
+                  },
+                ])
+                .then((choice) => {
+                  //declare empty variable to use in if statement
+                  let chosenManager;
+                  if (choice.manager) {
+                    //let chosenManager = choice
+                    chosenManager = choice.manager;
+                  } else {
+                    //let chosen manager = null
+                    chosenManager = null;
+                  }
+                  //push chosenManager to Employee Array
+                  newEmplArray.push(chosenManager);
+                  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+                  Values (?, ?, ?, ?)`;
+                  db.query(sql, newEmplArray, (err, res) => {
+                    if (err) {
+                      console.log(err);
+                    }
+                    //Success Message
+                    console.log("Employee successfully added to roster!");
+
+                    manageEmployees();
+                  });
+                });
+            });
+          });
+      });
+    });
 };
