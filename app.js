c969b61a1e07f96c3f353e27b09039823dcdc70b
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-08-12 11:26:52 -0400

    view by dept and by manager complete

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+// import all dependencies --inquirer, console.table and db connection from ./db/connection
 const inquirer = require("inquirer");
-const db = require("./db/connection");
 const cTable = require("console.table");
+const db = require("./db/connection");
 
+//connect to sql database, if error, log error, else run manageEmployees function
 db.connect((err) => {
   if (err) {
     console.log(err);
   }
+  //call function to start app
   manageEmployees();
 });
 
+//write function to start app - inquirer "list" of question, then return outcome for each selection
 const manageEmployees = () => {
   inquirer
     .prompt([
@@ -27,16 +31,16 @@ const manageEmployees = () => {
         ],
       },
     ])
-    .then((req) => {
-      const { interface } = req;
+    .then((choice) => {
+      //deconstruct "name:" and save as choice parameter
+      const { interface } = choice;
 
       if (interface === "View All Employees") {
         return viewAllEmployees();
       } else if (interface === "View All Employees By Department") {
-        console.log("Viewsdkjfnsdfl");
-        return "View All Employees By Department";
+        return viewAllByDepartment();
       } else if (interface === "View All Employees By Manager") {
-        return "View All Employees By Manager";
+        return viewAllByManager();
       } else if (interface === "Add Employee") {
         return "Add Employee";
       } else if (interface === "Remove Employee") {
@@ -47,6 +51,7 @@ const manageEmployees = () => {
         return "Update Employee Manager";
       }
     })
+    //catch errors in returns if exists
     .catch((err) => {
       console.log(err);
     });
@@ -54,6 +59,7 @@ const manageEmployees = () => {
 
 // view all employees function
 const viewAllEmployees = () => {
+  //save mysql query as sql to use in actual query below
   const sql = `SELECT employee.id AS id,
                       employee.first_name,
                       employee.last_name,
@@ -65,12 +71,85 @@ const viewAllEmployees = () => {
                       LEFT JOIN role ON employee.role_id = role.id
                       LEFT JOIN department ON role.department_id = department.id
                       LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+  //databse query (sql query, callback function)
+  db.query(sql, (err, res) => {
+    //catch errors if any exist
+    if (err) {
+      console.log(err);
+    }
+    //construct table in node.js from query
+    console.table(res);
+    //call initializing function to return to inquirer prompt
+    manageEmployees();
+  });
+};
+
+// view employees by Department
+const viewAllByDepartment = () => {
+  db.query(`SELECT * FROM department`, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    const chooseDept = res.map(({ id, dept_name }) => ({
+      name: dept_name,
+      value: id,
+    }));
+
+    inquirer
+      .prompt({
+        type: "list",
+        name: "department",
+        message: "Which department would you like to see?",
+        choices: chooseDept,
+      })
+      .then((choice) => {
+        const { department } = choice;
+        const sql = `SELECT CONCAT(first_name," ",last_name) AS Name
+                  FROM employee
+                    LEFT JOIN role ON role.id = employee.role_id
+                    LEFT JOIN department ON department.id = role.department_id
+                    WHERE department_id=?`;
+
+        db.query(sql, department, (err, res) => {
+          if (err) {
+            console.log(err);
+          }
+          console.table(res);
+          manageEmployees();
+        });
+      });
+  });
+};
+
+const viewAllByManager = () => {
+  const sql = `SELECT * FROM employee WHERE manager_id = 36`;
 
   db.query(sql, (err, res) => {
     if (err) {
       console.log(err);
     }
-    console.table(res);
-    manageEmployees();
+    const chooseManager = res.map(({ id, first_name, last_name }) => ({
+      name: first_name + " " + last_name,
+      value: id,
+    }));
+
+    inquirer
+      .prompt({
+        type: "list",
+        name: "managerList",
+        message: "Which manager's team would you like to see?",
+        choices: chooseManager,
+      })
+      .then((choice) => {
+        const { managerList } = choice;
+        const sql = `SELECT CONCAT(first_name," ",last_name) AS Name FROM employee WHERE manager_id=?`;
+        db.query(sql, managerList, (err, res) => {
+          if (err) {
+            console.log(err);
+          }
+          console.table(res);
+          manageEmployees();
+        });
+      });
   });
 };
